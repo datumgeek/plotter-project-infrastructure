ab6b843441deb53ce0ce42036a0a5b18c884b876
datumgeek <Mike.Graham@ihs.com>
2017-05-10 03:40:41 -0600

    Suppliers service working against sqlserver docker image

--- plotter-project-infrastructure/NorthwndDockerContainer/entrypoint.sh
+++ plotter-project-infrastructure/NorthwndDockerContainer/entrypoint.sh
@@ -1,3 +1,5 @@
 #start SQL Server and start the script to restore the database backup
 /opt/mssql/bin/sqlservr.sh & /usr/src/app/import-data.sh
+
+#hack to keep sql server container from exiting
 while true; do sleep 10s; done
--- plotter-project-infrastructure/NorthwndDockerContainer/setup.sql
+++ plotter-project-infrastructure/NorthwndDockerContainer/setup.sql
@@ -4,3 +4,9 @@ USE DemoData
 GO
 CREATE TABLE Products (ID int, ProductName nvarchar(max));
 GO
+RESTORE DATABASE Northwnd
+	FROM DISK = '/usr/src/app/Northwind.bak'
+	WITH
+		MOVE 'Northwind' TO '/usr/src/app/NORTHWND.mdf',
+		MOVE 'Northwind_log' TO '/usr/src/app/NORTHWND.ldf';
+GO
