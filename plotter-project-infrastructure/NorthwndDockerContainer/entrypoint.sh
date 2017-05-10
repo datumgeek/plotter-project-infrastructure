0738b83c1098fb343d8f95f4a88096ba1ea6ad5f
datumgeek <Mike.Graham@ihs.com>
2017-05-10 01:58:21 -0600

    sql server docker container working !!

--- plotter-project-infrastructure/NorthwndDockerContainer/entrypoint.sh
+++ plotter-project-infrastructure/NorthwndDockerContainer/entrypoint.sh
@@ -1,2 +1,3 @@
 #start SQL Server and start the script to restore the database backup
 /opt/mssql/bin/sqlservr.sh & /usr/src/app/import-data.sh
+while true; do sleep 10s; done
--- plotter-project-infrastructure/NorthwndDockerContainer/import-data.sh
+++ plotter-project-infrastructure/NorthwndDockerContainer/import-data.sh
@@ -2,7 +2,7 @@
 sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Yukon900 -d master -i setup.SQL
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Yukon900 -d master -i setup.sql
 
 #import the data from the csv file
 /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U SA -P Yukon900
